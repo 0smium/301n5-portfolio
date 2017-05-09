a503c6db3ed0d0604cd61b720a1936c9dbf73201
0smium <steve.walsh@transfueler.com>
2017-05-09 16:36:25 -0700

    successfully populating demo projects

---
+++ js/projects.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var projectData = [
+  {
+    title:        'Demo Project 1',
+    description:  'Demo Description 1',
+    url:          'http://www.google.com',
+    publishedOn:  '2017-01-31'
+  },
+  {
+    title:        'Demo Project 2',
+    description:  'Demo Description 2',
+    url:          'http://www.google.com',
+    publishedOn:  '2017-02-10'
+  },
+  {
+    title:        'Demo Project 3',
+    description:  'Demo Description 3',
+    url:          'http://www.google.com',
+    publishedOn:  '2017-02-20'
+  },
+];
