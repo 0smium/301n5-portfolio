9edfca929ca80ae53c168dc9cc00fca40b09a971
0smium <steve.walsh@transfueler.com>
2017-05-11 17:02:37 -0700

    toggling works, expanded descriptions to add additional p tags, modified a little CSS.

--- js/projects.js
+++ js/projects.js
@@ -3,28 +3,28 @@
 var projectData = [
   {
     title:        'Biographic Page',
-    description:  'This is a basic resume/biographic page that lets the reader play a simple JavaScript guessing game based on the content.',
+    description:  '<p>A basic resume/biographic page using vanilla JavaScript.</p><p>The page includes W3C validated semantic HTML5 and CSS.  It lets the reader play a simple JavaScript guessing game based on the content using basic <em>alerts</em> and <em>prompts</em>.</p>',
     url:          'https://0smium.github.io/bio_game/',
     imgUrl:       'images/bio_game.jpg',
     publishedOn:  '2017-01-26'
   },
   {
     title:        'Algae Cookies',
-    description:  'This is a demo site for a ficticious "algae cookie" store that populates demo data for various locations.',
+    description:  '<p>A demo site for a ficticious "algae cookie" store.</p><p>The app populates demo projections data for various locations in Seattle.</p>',
     url:          'https://0smium.github.io/cookie-stand/',
     imgUrl:       'images/algae_cookies.jpg',
     publishedOn:  '2017-03-02'
   },
   {
     title:        'Bus Mall',
-    description:  'This is a demo site for a bus version of "Sky Mall" that determines what products are most popular and charts the results.',
+    description:  '<p>A demo site for a bus version of <em>Sky Mall</em>.</p><p>The app determines what products are most popular and charts the results using the Chart.js library.</p>',
     url:          'https://0smium.github.io/bus-mall/',
     imgUrl:       'images/bus_mall.jpg',
     publishedOn:  '2017-03-04'
   },
   {
     title:        'Black Jack',
-    description:  'This is a fully functional Black Jack game coded strictly in vanilla JavaScript.  It features a sleek, modern design and incorporates the abity to altr bets and double-down.  It utilizes local storage for a basic leaderboard.',
+    description:  '<p>A fully functional Black Jack game coded strictly in vanilla JavaScript.</p><p>This was a group project for the completion of Code 301.  It features a sleek, modern design and incorporates the abity to alter bets and double-down.  It also utilizes local storage for a basic leaderboard.</p>',
     url:          'https://0smium.github.io/BlackJack/',
     imgUrl:       'images/blackjack.jpg',
     publishedOn:  '2017-03-18'
