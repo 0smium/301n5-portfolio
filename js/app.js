e7a9e12dc3ed00ae2421e674c743a3e4863e784a
0smium <steve.walsh@transfueler.com>
2017-05-15 17:37:54 -0700

    fixed plain text issue by using triple brackets

--- js/app.js
+++ js/app.js
@@ -16,28 +16,8 @@ function Project (projectDataObj) {
 Project.prototype.toHtml = function() {
   var templateScript = $('#article-template').html();
   var template = Handlebars.compile(templateScript);
-
-  // var $newProject = $('article.template').clone();
-
-  // $newProject.removeClass('template');
-
-  // if (!this.publishedOn) $newProject.addClass('draft');
-  // $newProject.attr('data-category', this.category);
-
-    // jQuery appends the DOM for properties above.
-  // $newProject.find('a').attr('href', this.url);
-  // $newProject.find('img').attr('alt', this.description);
-  // $newProject.find('img').attr('src', this.imgUrl);
-  // $newProject.find('h2').text(this.title);
-  // $newProject.find('section.project-description').html(this.description);
-  // $newProject.find('time').attr('datetime', this.publishedOn);
-
-  // Display the date as a relative number of 'days ago'
-  // $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago.');
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? `about ${this.daysAgo} days ago.` : '(draft)';
-  // $newProject.append('<hr>');
-  // return $newProject;
+  this.publishStatus = this.publishedOn ? `Published about ${this.daysAgo} days ago.` : '(draft)';
   return template(this)
 };
 
